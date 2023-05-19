6b16e1b41170ae09f2c7c0c0a3c3b03bb6ed9c3c
Ursula Armstrong <cukimonkey@gmail.com>
2023-05-19 11:52:13 +0100

    Passing data to our App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
-// import SetupForm from './Form/SetupForm';
-// import Modal from './Modal/Modal';
-// import Loading from './Loading/LoadingScreen';
+ import SetupForm from './Form/SetupForm';
+import Modal from './Modal/Modal';
+import Loading from './Loading/LoadingScreen';
+import { useGlobalContext } from './Context/Context';
+import SetupForm from './Form/SetupForm';
 
 function App() {
+const {waiting, loading, questions, index, correct, nextQuestion, checkAnswer} = useGlobalContext;
+
+if(waiting){
+  return (<SetupForm />)
+}
+if(loading){
+  return (<Loading />)
+}
+//now refactor our answers we receiving
+
+const {incorrect_answers, correct_answer, question} = question[index];
+let answers = [...incorrect_answers];
+const tempIndex = Math.floor(Math.random()*4);
+if (tempIndex === 3) {
+  answers.push(correct_answer);
+} else {
+  answers.push(answers[tempIndex]);
+  answers[tempIndex] = correct_answer;
+}
+
+
   return (
     //conditionally render components
     <main className="App">
         <section className='quiz'>
-          <p className='correct-answers'>Correct answers: 3</p>
+          <p className='correct-answers'>Correct answers: {correct}/{index}</p>
           <article className='container'>
-            <h2>Text</h2>
-            <div className='btn-container'></div>
+            <h2 dangerouslySetInnerHTML={{__html:question}}/>
+            <div className='btn-container'>
+              {answers.map((answer, index) => {
+                retunr (
+                  <button 
+                  key={index} 
+                  className='answer-btn' 
+                  onClick={()=> checkAnswer(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{__html:answer}} 
+                  />
+                )
+              })}
+            </div>
           </article>
           <button className='next-question'>Next Question</button>
         </section>
